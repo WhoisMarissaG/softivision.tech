cda2bf1b2462883715a2112ac03118f7b33d823a
Marissa <99129548+WhoisMarissaG@users.noreply.github.com>
2022-09-08 12:48:53 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -126,7 +126,7 @@ const displacementSlider = function(opts) {
                     document.getElementById('pagination').querySelectorAll('.active')[0].className = '';
                     this.className = 'active';
 
-                    let slideId = parseInt( this.dataset.slide, 10 );
+                    let slideId = parseInt( this.dataset.slide, 12 );
 
                     mat.uniforms.nextImage.value = sliderImages[slideId];
                     mat.uniforms.nextImage.needsUpdate = true;
