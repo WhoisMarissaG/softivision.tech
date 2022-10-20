9a17a561c0559dba7f53360a096c126a87271137
Marissa <99129548+WhoisMarissaG@users.noreply.github.com>
2022-10-20 09:43:27 +0200

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-     gsap.set("main article *", { autoAlpha: 0, y: "1rem" });
+gsap.set("main article *", { autoAlpha: 0, y: "1rem" });
 
 const animateVisible = (block, ratio, isIntersecting) => {
   if (ratio > 0 && isIntersecting) {
@@ -7,27 +7,27 @@ const animateVisible = (block, ratio, isIntersecting) => {
       autoAlpha: 1,
       y: "0",
       stagger: 0.3,
-      ease: "power3.inOut"
-    });
+      ease: "power3.inOut" });
+
   } else {
     gsap.set(block.querySelectorAll("*"), {
       autoAlpha: 0,
-      y: "1rem"
-    });
+      y: "1rem" });
+
   }
 };
 
 const blocks = document.querySelectorAll("main article");
 
 const blocksObserver = new IntersectionObserver(
-  (entries) => {
-    return entries.forEach((event) => {
-      const { target, intersectionRatio, isIntersecting } = event;
-      animateVisible(target, intersectionRatio, isIntersecting);
-    });
-  },
-  { threshold: 0.5 }
-);
+entries => {
+  return entries.forEach(event => {
+    const { target, intersectionRatio, isIntersecting } = event;
+    animateVisible(target, intersectionRatio, isIntersecting);
+  });
+},
+{ threshold: 0.5 });
+
 
 for (const block of blocks) {
   blocksObserver.observe(block);
